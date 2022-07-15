4d8a656bf347662d04ce79734947414bda5b0b77
edlyn castro <edlyn.castro@neoris.com>
2022-07-15 16:47:30 -0600

    cambios boton

--- WordToPdf/WordToPdf/Form1.Designer.cs
+++ WordToPdf/WordToPdf/Form1.Designer.cs
@@ -38,34 +38,38 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(86, 180);
+            this.label1.Location = new System.Drawing.Point(115, 222);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.Size = new System.Drawing.Size(61, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mensaje";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(82, 94);
+            this.button1.Location = new System.Drawing.Point(109, 116);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Convertir";
+            this.button1.Text = "Convertir2";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(137, 178);
+            this.textBox1.Location = new System.Drawing.Point(183, 219);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(705, 20);
+            this.textBox1.Size = new System.Drawing.Size(939, 22);
             this.textBox1.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(320, 93);
+            this.button2.Location = new System.Drawing.Point(427, 114);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(114, 23);
+            this.button2.Size = new System.Drawing.Size(152, 28);
             this.button2.TabIndex = 3;
             this.button2.Text = "Convertir Microsoft";
             this.button2.UseVisualStyleBackColor = true;
@@ -73,9 +77,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(510, 93);
+            this.button3.Location = new System.Drawing.Point(680, 114);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(100, 28);
             this.button3.TabIndex = 4;
             this.button3.Text = "Exportar";
             this.button3.UseVisualStyleBackColor = true;
@@ -83,14 +88,15 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(865, 278);
+            this.ClientSize = new System.Drawing.Size(1153, 342);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Convertir a PDF";
             this.ResumeLayout(false);
